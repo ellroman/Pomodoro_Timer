c67aea6dc41cb759c61469aac2b4aa75e776acdf
ellroman <74682044+ellroman@users.noreply.github.com>
2024-01-04 12:11:33 -0500

    Add files via upload

---
+++ Pomodoro/js/index.js
@@ -0,0 +1,28 @@
+// Nav Functions
+const timer = new Timer();
+timer.reset();
+
+const action = (action = "stop")=>{
+    switch(action.toLowerCase()){
+        case "start":
+            timer.start();
+            break;
+        default:
+            timer.stop();
+            break;
+    }
+}
+
+const navLinks = document.querySelectorAll(".nav > ul > li");
+const navBg = document.getElementById('indicator');
+
+
+navLinks.forEach((navItem, i)=>{
+    navItem.addEventListener('click',()=>{
+        navLinks.forEach((nav)=>{
+            nav.classList.remove('active')
+            navBg.style.marginLeft = `calc(calc(100%/3)*${i})`;
+            navItem.classList.add('active');
+        });
+    });
+});
---
+++ Pomodoro/js/timer.js
@@ -0,0 +1,77 @@
+class Timer{
+    constructor(type = "pomodoro") {
+        this.type = type
+        this.pomodoro = 25;
+        this.shortbreak = 5;
+        this.longbreak = 15;
+        this.timeElement = window[type];
+        this.clock = document.getElementById('time');
+        this.actionElement = document.getElementById('action');
+        this.time = this.timeElement;
+        this.text = this.time <= 9 ? `0${this.time}` : `${this.time}`;
+        this.circle = document.querySelector("#ring > circle");
+    }
+
+    select(type){
+    this.type = type;
+    this.reset()
+    }
+
+    
+    start(){
+        const format = (time)=>(time<10?"0"+time : time);
+        let time = this[this.type] * 60;
+
+        this.clock.innerText = `${this.text}:00`;
+        this.circle.style.strokeDashoffset = 1024;
+
+        let startTime = time;
+        let minutes = 0;
+        let seconds = 0;
+
+        time--
+        this.interval = setInterval(()=>{
+            minutes = Math.floor(time/60);
+            seconds = Math.floor(time % 60);
+
+            minutes = format(minutes);
+            seconds = format(seconds);
+
+            this.clock.innerText = `${minutes}:${seconds}`;
+            const percent = ((time%startTime/startTime))*100;
+            const offset = (percent/100)*1024;
+
+            this.circle.style.strokeDashoffset = offset;
+            
+            if(--time < 0){
+                time = 0;
+                clearInterval(this.interval);
+                this.actionElement.innerText = "reset";
+            }
+        },1000);
+
+        this.actionElement.innerText = "stop"
+    }
+
+    reset() {
+        this.stop()
+        this.circle.style.strokeDashoffset = 1024;
+        this.time = this[this.type];
+        this.text = this.time <= 9 ? `0${this.time}` : `${this.time}`;
+        this.actionElement.innerText = "start";
+        this.clock.innerText = `${this.text}:00`;
+    }
+
+    stop(){
+        clearInterval(this.interval);
+        this.actionElement.innerText = "start";
+    }
+}
+        
+
+
+
+ 
+
+
+
